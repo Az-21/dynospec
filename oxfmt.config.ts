4431fc336516ba4aba058ddc0cdb5e4ab14cc7cb
Abhishek Choudhary <github@az21.simplelogin.com>
2026-09-05 17:48:03 +0530

    chore: re-initialize project

---
+++ oxfmt.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  printWidth: 120,
+  semi: true,
+  singleQuote: false,
+  sortImports: true,
+  sortPackageJson: true,
+  sortTailwindcss: true,
+  svelte: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+});
