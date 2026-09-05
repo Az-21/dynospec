41174075cc2afd51ba808ccd12913dc09f7abc5c
Abhishek Choudhary <github@az21.simplelogin.com>
2026-09-06 00:00:32 +0530

    feat: create fixed DB for FH6

---
+++ src/lib/data/forza/forza-horizon-6.ts
@@ -0,0 +1,53 @@
+import { ForzaTuneSchema } from "$lib/types/forza";
+import { z } from "zod";
+
+const _tunes: z.input<typeof ForzaTuneSchema>[] = [
+  {
+    car: {
+      make: "Toyota",
+      model: "Supra",
+      year: 2010,
+    },
+    tune: {
+      creator: "KapienPL",
+      comment: null,
+      leaderboard: null,
+      difficulty: 1,
+      speed: 1,
+      handling: 1,
+      meta: 1,
+    },
+    forza: {
+      class: "D",
+      shareCode: "123 456 789",
+      createdOn: "2026-08-10",
+      discipline: "Cross Country",
+      drivetrain: "AWD",
+    },
+  },
+  {
+    car: {
+      make: "Nissan",
+      model: "Skyline GT-R (R34)",
+      year: 2005,
+    },
+    tune: {
+      creator: "ESV Mars",
+      comment: null,
+      leaderboard: "Track XYZ | 01:23 | Top 3k",
+      difficulty: 2,
+      speed: 2,
+      handling: 2,
+      meta: 2,
+    },
+    forza: {
+      class: "A",
+      shareCode: "456 456 789",
+      createdOn: "2026-09-01",
+      discipline: "Road",
+      drivetrain: "AWD",
+    },
+  },
+];
+
+export const forzaHorizon6Tunes = ForzaTuneSchema.array().parse(_tunes);
